a83731d1783ccfa6023ee2b8fdd744a3bb707974
Mrtracker-new <85892183+Mrtracker-new@users.noreply.github.com>
2025-12-19 01:34:42 +0530

    build(docker): install yt-dlp from master branch

--- server/install-ytdlp.sh
+++ server/install-ytdlp.sh
@@ -2,7 +2,7 @@
 # Install yt-dlp for video downloading
 
 echo "Installing yt-dlp..."
-pip install yt-dlp
+pip install https://github.com/yt-dlp/yt-dlp/archive/master.zip
 
 echo "yt-dlp installed successfully!"
 yt-dlp --version
