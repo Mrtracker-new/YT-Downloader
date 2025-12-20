d734e4bb9d473c2ff5c636c6e6ca9ecfe7c075c0
Mrtracker-new <85892183+Mrtracker-new@users.noreply.github.com>
2025-12-20 20:50:50 +0530

    Delete those dusty old batch files - Marie Kondo would be proud, they didn't spark joy

--- start-frontend.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Starting Frontend Server...
-cd client
-start "YouTube Downloader Frontend" cmd /k "npm run dev"
-echo Frontend server starting in new window...
-echo Application will open at http://localhost:3000
-pause
